860465370b6bc717140df624062773d737b9d98f
Rana M. Ajmal <rana.ajmal@gmail.com>
2024-09-30 13:07:24 +0500

    init file structure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,59 @@
-import { Stack } from 'expo-router';
-import { StyleSheet, View, Text } from 'react-native';
+import {useState} from 'react';
+import {View} from "react-native";
 
-import movies from "~/assets/data/movies.json"; 
-import { ScreenContent } from '~/components/ScreenContent';
+import Nav from '~/components/movie-list/Nav';
+import NavItem from '~/components/movie-list/NavItem';
+// import List from '~/components/movie-list/List';
+// import ListItem from '~/components/movie-list/ListItem';
 
-export default function Home() {
+import collections from "~/assets/data/collections.json";
+// import movies from "~/asssets/data/movies.json";
+
+export default function Movies() {
+  const [selectedCollection, setSelectedCollection] = useState('');
+
   return (
-    <>
-      <Stack.Screen options={{ title: 'Movie List' }} />
-      <View style={styles.container}>
-        <Text>{JSON.stringify(movies)}</Text>
-      </View>
-    </>
-  );
+    <View className="divide-y divide-slate-100">
+      <Nav>
+        {collections.map(c => (
+          <NavItem 
+            key={c.id}
+            href="/new"
+            isActive={c === selectedCollection}>
+              {c.name}
+          </NavItem>
+        ))}
+      </Nav>
+      {/* <List>
+        {movies.map((movie) => (
+          <ListItem key={movie.id} movie={movie} />
+        ))}
+      </List> */}
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
+
+// import { Stack } from 'expo-router';
+// import { StyleSheet, View, Text } from 'react-native';
+
+// import movies from "~/assets/data/movies.json"; 
+// import { ScreenContent } from '~/components/ScreenContent';
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Stack.Screen options={{ title: 'Movie List' }} />
+//       <View style={styles.container}>
+//         <Text>{JSON.stringify(movies)}</Text>
+//       </View>
+//     </>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 24,
+//   },
+// });
